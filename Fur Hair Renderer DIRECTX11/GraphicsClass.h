b38ee4e5ca5b23d0d341ff29f1adb82769c8546f
Vivinator <vivienne2.nip@live.uwe.ac.uk>
2018-03-28 16:38:12 +0100

    First commit

---
+++ Fur Hair Renderer DIRECTX11/GraphicsClass.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+
+#include "d3dclass.h"
+#include "cameraclass.h"
+#include "modelclass.h"
+#include "colorshaderclass.h"
+
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+	void MoveCamera(float x, float y, float z);
+
+private:
+	bool Render();
+	CameraClass* m_Camera;
+	ModelClass* m_Model;
+	ColorShaderClass* m_ColorShader;
+
+private:
+	D3DClass* m_D3D;
+};
+
+#endif
+
